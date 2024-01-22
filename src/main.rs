17d1b716b63457ac33650d1ab4951e5b36f535d3
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-01-23 00:17:27 +0530

    added image generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use image::Luma;
+use qrcode::QrCode;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let link = QrCode::new(b"https://twitter.com/home").unwrap();
+
+    let image = link.render::<Luma<u8>>().build();
+
+    image.save("./output/qrcode.png").unwrap();
+
+    Command::new("open")
+        .arg("./output/qrcode.png")
+        .spawn()
+        .expect("failed to open qr code.");
 }
